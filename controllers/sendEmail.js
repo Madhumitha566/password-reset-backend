de569db12fb403d6b2d15f46d867f2e5847c0794
madhumitha566 <madhumithamurugan8204@gmail.com>
2026-05-04 14:34:57 +0530

    commit

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (email, subject, message) => {
   try {
     const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // Note: Use an App Password for Gmail
