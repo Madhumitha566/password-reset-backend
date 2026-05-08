f1b6899fd2484282006ea673be293878e90296e0
madhumitha566 <madhumithamurugan8204@gmail.com>
2026-05-08 17:42:34 +0530

    forgot commit

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,36 +1,3 @@
-/* import { BrevoClient, BrevoError } from '@getbrevo/brevo';
-import dotenv from 'dotenv'
-dotenv.config()
-const brevo = new BrevoClient({
-  apiKey: process.env.BREVO_API_KEY, 
-});
-
-
-const BREVO_LOGIN_EMAIL = process.env.EMAIL_FROM;
-
-
-const sendEmail=async ({ to, subject, content })=> {
-  try {
-    const result = await brevo.transactionalEmails.sendTransacEmail({
-      subject: subject,
-      textContent: content,
-      sender: { name: "My App", email: BREVO_LOGIN_EMAIL },
-      to: [{ email: to }]
-    });
-
-    return { success: true, messageId: result.messageId };
-
-  } catch (err) {
-    if (err instanceof BrevoError) {
-      console.error(`Brevo Error [${err.statusCode}]: ${err.message}`);
-    } else {
-      console.error('Unexpected Error:', err);
-    }
-    
-    return { success: false, error: err.message };
-  }
-}
-export default sendEmail */
 import { BrevoClient, BrevoError } from '@getbrevo/brevo';
 import dotenv from 'dotenv';
 
