c8efabe9808196017f5f3dc505366d8f4b15bb75
madhumitha566 <madhumithamurugan8204@gmail.com>
2026-05-12 15:58:09 +0530

    to

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -84,7 +84,7 @@ export const forgotPassword = async (req, res) => {
 
     // 4. Trigger the Brevo API via our sendEmail utility
     await sendEmail({
-      to: req.user?.email || process.env.EMAIL_FROM,
+      to: user.email || process.env.EMAIL_FROM,
       subject: subject,
       html: htmlContent
     });
