a66b050f53ef2a22fd4ba528d2befb45756d445d
madhumitha566 <madhumithamurugan8204@gmail.com>
2026-05-12 14:57:50 +0530

    brevo send

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,4 +1,4 @@
-import { BrevoClient, BrevoError } from '@getbrevo/brevo';
+/* import { BrevoClient, BrevoError } from '@getbrevo/brevo';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -46,4 +46,81 @@ const sendEmail = async ({ to, subject, html }) => {
   }
 };
 
-export default sendEmail;
+export default sendEmail; */
+
+
+// Include the Brevo library
+/* import SibApiV3Sdk from 'sib-api-v3-sdk';
+
+// Initialize and configure the Brevo Client
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY; 
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+
+ const sendEmail = (userLoginEmail) => {
+  var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+  sendSmtpEmail.subject = "My subject";
+  sendSmtpEmail.htmlContent = "<html><body><h1>Congratulations!</h1><p>You successfully sent this transactional email via the Brevo API.</p></body></html>";
+
+  sendSmtpEmail.sender = { 
+    "name": "From Name", 
+    "email": process.env.EMAIL_FROM 
+  };
+
+  sendSmtpEmail.to = [{ 
+    "email": userLoginEmail, 
+  }];
+
+  // Make the call to the Brevo API and return the promise
+  return apiInstance.sendTransacEmail(sendSmtpEmail)
+    .then(function(data) {
+      console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+      return data;
+    })
+    .catch(function(error) {
+      console.error('Error sending email:', error);
+      throw error; 
+    });
+};
+export default sendEmail */
+import SibApiV3Sdk from 'sib-api-v3-sdk';
+
+// Initialize and configure the Brevo Client
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY; 
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+
+const sendEmail = ({ to, subject, html }) => {
+  var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+  sendSmtpEmail.subject = subject;
+  sendSmtpEmail.htmlContent = html;
+
+  sendSmtpEmail.sender = { 
+    "name": "My App", 
+    "email": process.env.EMAIL_FROM 
+  };
+
+  sendSmtpEmail.to = [{ 
+    "email": to 
+  }];
+
+  // Make the call to the Brevo API and return the promise
+  return apiInstance.sendTransacEmail(sendSmtpEmail)
+    .then(function(data) {
+      console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+      return data;
+    })
+    .catch(function(error) {
+      console.error('Error sending email:', error);
+      throw error; 
+    });
+};
+export default sendEmail
